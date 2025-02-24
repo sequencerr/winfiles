ba6d35383e1a9db15399e72fc91234369b68aeba
Sequencer <45060278+sequencerr@users.noreply.github.com>
2025-02-24 23:51:13 +0100

    Fix edge uninstall

---
+++ README.md
@@ -0,0 +1,4 @@
+### Acknowledgments
+
+- https://github.com/ChrisTitusTech/winutil
+- https://gist.github.com/ave9858/c3451d9f452389ac7607c99d45edecc6
--- functions/edge.ps1
+++ functions/edge.ps1
@@ -1,82 +1,42 @@
-function Invoke-EdgeClientUninstall {
-    param (
-        [Parameter(Mandatory = $true)]
-        [String]$Key
-    )
-
-    $originalNation = [Microsoft.Win32.Registry]::GetValue('HKEY_USERS\.DEFAULT\Control Panel\International\Geo', 'Nation', [Microsoft.Win32.RegistryValueKind]::String)
-
-    # Set Nation to any of the EEA regions temporarily
-    # Refer: https://learn.microsoft.com/en-us/windows/win32/intl/table-of-geographical-locations
-    $tmpNation = 68 # Ireland
-    [Microsoft.Win32.Registry]::SetValue('HKEY_USERS\.DEFAULT\Control Panel\International\Geo', 'Nation', $tmpNation, [Microsoft.Win32.RegistryValueKind]::String) | Out-Null
-
-    $baseKey = 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\EdgeUpdate'
-    $registryPath = $baseKey + '\ClientState\' + $Key
-
-    if (!(Test-Path -Path $registryPath)) {
-        Write-Host "[$Mode] Registry key not found: $registryPath"
-        return
-    }
-
-    # Remove the status flag
-    Remove-ItemProperty -Path $baseKey -Name "IsEdgeStableUninstalled" -ErrorAction SilentlyContinue | Out-Null
-    Remove-ItemProperty -Path $registryPath -Name "experiment_control_labels" -ErrorAction SilentlyContinue | Out-Null
-
-    $uninstallString = (Get-ItemProperty -Path $registryPath).UninstallString
-    $uninstallArguments = (Get-ItemProperty -Path $registryPath).UninstallArguments
-    $uninstallArguments += " --force-uninstall --delete-profile" # Extra arguments to nuke it
-
-    if ([String]::IsNullOrEmpty($uninstallString)) {
-        Write-Host "[$Mode] Cannot find uninstall methods for $Mode"
-        return
-    }
-    if (!(Test-Path -Path $uninstallString)) {
-        Write-Host "[$Mode] setup.exe not found at: $uninstallString"
-        return
-    }
-    Start-Process -FilePath $uninstallString -ArgumentList $uninstallArguments -Wait -NoNewWindow -Verbose
-
-    # Restore Nation back to the original
-    [Microsoft.Win32.Registry]::SetValue('HKEY_USERS\.DEFAULT\Control Panel\International\Geo', 'Nation', $originalNation, [Microsoft.Win32.RegistryValueKind]::String) | Out-Null
-
-    # might not exist in some cases
-    if ((Get-ItemProperty -Path $baseKey).IsEdgeStableUninstalled -eq 1) {
-        Write-Host "[$Mode] Edge Stable has been successfully uninstalled"
-    }
-}
-
 function Invoke-EdgeBrowserUninstall {
-    Remove-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft Edge" -Name "NoRemove" -ErrorAction SilentlyContinue | Out-Null
+    if (!(Test-Path -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft Edge")) { Write-Host "Edge is not installed."; return }
 
-    [Microsoft.Win32.Registry]::SetValue("HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\EdgeUpdateDev", "AllowUninstall", 1, [Microsoft.Win32.RegistryValueKind]::DWord) | Out-Null
+    $pathEdgeExe = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\msedge.exe")."(Default)"
 
-    Invoke-EdgeClientUninstall -Key '{56EB18F8-B008-4CBD-B6D2-8C97FE7E9062}'
+    # for some reason needed for uninstaller(UninstallString). without this, uninstaller silently exists
+    $pathEdgeUWP = "$env:SystemRoot\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe"
+    New-Item "$pathEdgeUWP" -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+    New-Item "$edgeUWP\MicrosoftEdge.exe" -ErrorAction SilentlyContinue | Out-Null
 
+    # Set-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\EdgeUpdateDev" -Name AllowUninstall -Value '' -ErrorAction SilentlyContinue
+    # Set-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft Edge\EdgeUpdate\ClientState\{56EB18F8-B008-4CBD-B6D2-8C97FE7E9062}" -Name experiment_control_labels -Value '' -ErrorAction SilentlyContinue # it was on old versions, instead EdgeUpdateDev.AllowUninstall
+
+    $uninstallString = (Get-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft Edge").UninstallString + " --force-uninstall --delete-profile"
+    Start-Process cmd.exe "/c $uninstallString" -WindowStyle Hidden -Wait
+
+    # EdgeUWP(Universal Windows Platform) is removed in latest patch of Windows. Before it broke Windows updates, if removal not handled correctly.
+    Remove-Item "$pathEdgeUWP" -Recurse -ErrorAction SilentlyContinue | Out-Null
+
+    # Leftovers
+    # Note: The Microsoft Edge Update service might remain, this is normal as it is required for updating WebView2.
     Remove-Item -Path "Computer\\HKEY_CLASSES_ROOT\\MSEdgePDF" -ErrorAction SilentlyContinue | Out-Null
     Remove-Item -Path "Computer\\HKEY_CLASSES_ROOT\\MSEdgeHTM" -ErrorAction SilentlyContinue | Out-Null
     Remove-Item -Path "Computer\\HKEY_CLASSES_ROOT\\MSEdgeMHT" -ErrorAction SilentlyContinue | Out-Null
     Remove-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Recurse -ErrorAction SilentlyContinue | Out-Null
     Remove-Item -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Edge" -Recurse -ErrorAction SilentlyContinue | Out-Null
 
-    # FIXME: might not work on some systems
-    # function Invoke-EdgeUpdateUninstall
-    Remove-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft Edge Update" -Name "NoRemove" -ErrorAction SilentlyContinue | Out-Null
-
-    $registryPath = 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\EdgeUpdate'
-    if (!(Test-Path -Path $registryPath)) {
-        Write-Host "Registry key not found: $registryPath"
-        return
+    $sh = New-Object -ComObject WScript.Shell
+    foreach ($shortcut in (Get-ChildItem -Path ([Environment]::GetFolderPath("Desktop")) -File -Filter "*.lnk")) {
+        try {
+            if ($pathEdgeExe -ne $sh.CreateShortcut($shortcut.FullName).TargetPath) { continue }
+            Write-Host "Found shortcut on desktop: $($shortcut.FullName)"
+            Remove-Item "$($shortcut.FullName)"
+        } catch {
+            Write-Host "Failed to read shortcut target for: $($shortcut.FullName)"
+        }
     }
 
-    $uninstallCmdLine = (Get-ItemProperty -Path $registryPath).UninstallCmdLine
-    if ([String]::IsNullOrEmpty($uninstallCmdLine)) {
-        Write-Host "Cannot find uninstall methods for $Mode"
-        return
-    }
-
-    Start-Process cmd.exe "/c $uninstallCmdLine" -WindowStyle Hidden -Wait
-    Remove-Item -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\EdgeUpdate" -Recurse -ErrorAction SilentlyContinue | Out-Null
+    Write-Host "Edge Stable has been successfully uninstalled"
 }
 
 function Invoke-EdgeInstall {
