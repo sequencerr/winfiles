d41a186b89fef7f6e45baf864e920efb37a8523f
Sequencer <45060278+sequencerr@users.noreply.github.com>
2025-02-24 17:49:55 +0100

    Fix sourcing

--- post-install.ps1
+++ post-install.ps1
@@ -1,4 +1,4 @@
-. .\functions\*
+Get-ChildItem .\functions\ -File | ForEach-Object { . $_.FullName }
 
 Invoke-UpdatesDisable
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Program Files\Git\bin", [System.EnvironmentVariableTarget]::Machine)
