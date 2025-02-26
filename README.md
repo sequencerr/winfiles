eb91f3115ee3f7d85a11e80100cfc28e0a3c5526
Sequencer <45060278+sequencerr@users.noreply.github.com>
2025-02-26 05:22:14 +0100

    Add remove apps

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 - https://github.com/ChrisTitusTech/winutil
 - https://gist.github.com/ave9858/c3451d9f452389ac7607c99d45edecc6
+- https://github.com/Raphire/Win11Debloat
+- https://answers.microsoft.com/en-us/windows/forum/all/uninstall-pre-not-installed-spotify-whatsapp-etc/328d585c-9764-4407-9fac-f57c78b0dade
+- https://learn.microsoft.com/en-us/answers/questions/750757/how-to-uninstall-entire-linkedin-from-windows-10-p
--- post-install.ps1
+++ post-install.ps1
@@ -4,3 +4,49 @@ Invoke-UpdatesDisable
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Program Files\Git\bin", [System.EnvironmentVariableTarget]::Machine)
 
 Invoke-EdgeBrowserUninstall
+
+Get-AppxPackage -AllUsers | Where-Object { (Get-AppxPackage $_.Name) -ne $null -and !(Get-AppxPackage $_.Name).NonRemovable } | Select Name, PackageFullName | Sort-Object { $_.Name }
+$programs = @(
+    "Microsoft.BingSearch"
+    "Microsoft.BingWeather"
+    "Microsoft.Copilot"
+    "Microsoft.GetHelp"
+    "Microsoft.GetStarted"
+    "Microsoft.Microsoft3DViewer"
+    "Microsoft.MicrosoftOfficeHub"
+    "Microsoft.MicrosoftSolitaireCollection"
+    "Microsoft.MicrosoftStickyNotes"
+    "Microsoft.MixedReality.Portal"
+    "Microsoft.Office.OneNote"
+    "Microsoft.OutlookForWindows"
+    "Microsoft.People"
+    "Microsoft.ScreenSketch"
+    "Microsoft.SkypeApp"
+    "Microsoft.StorePurchaseApp"
+    "Microsoft.Wallet"
+    "Microsoft.Windows.DevHome"
+    "Microsoft.Windows.Photos"
+    "Microsoft.WindowsAlarms"
+    "Microsoft.WindowsCamera"
+    "microsoft.windowscommunicationsapps"
+    "Microsoft.WindowsFeedbackHub"
+    "Microsoft.WindowsMaps"
+    "Microsoft.WindowsSoundRecorder"
+    "Microsoft.WindowsStore"
+    "Microsoft.Xbox.TCUI"
+    "Microsoft.XboxApp"
+    "Microsoft.XboxGameOverlay"
+    "Microsoft.XboxGamingOverlay"
+    "Microsoft.XboxIdentityProvider"
+    "Microsoft.XboxSpeechToTextOverlay"
+    "Microsoft.YourPhone"
+    "Microsoft.ZuneMusic"
+    "Microsoft.ZuneVideo"
+)
+foreach ($program in $programs) {
+    if ($null -eq (Get-AppxPackage -Name $program)) { continue }
+    if ((Get-AppxPackage -Name $program).NonRemovable) { Write-Error "$program is `"NonRemovable`""; return }
+    Write-Output "Removing $program..."
+    Get-AppxPackage -Name $program | Remove-AppxPackage
+    Get-AppxProvisionedPackage -Online | Where-Object DisplayName -eq $program | Remove-AppxProvisionedPackage -Online
+}
