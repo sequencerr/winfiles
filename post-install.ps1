82e0f982e2dce97a7e8024dcab75842b6de6b6b5
Sequencer <45060278+sequencerr@users.noreply.github.com>
2025-02-26 05:22:28 +0100

    Rm export env var

--- post-install.ps1
+++ post-install.ps1
@@ -1,7 +1,6 @@
 Get-ChildItem .\functions\ -File | ForEach-Object { . $_.FullName }
 
 Invoke-UpdatesDisable
-[Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Program Files\Git\bin", [System.EnvironmentVariableTarget]::Machine)
 
 Invoke-EdgeBrowserUninstall
 
