da6096fd0c269929465cecf66486b4b1e8edaca9
Sequencer <45060278+sequencerr@users.noreply.github.com>
2025-03-15 13:54:53 +0100

    Add Self-elevating administrator rights

--- post-install.ps1
+++ post-install.ps1
@@ -1,3 +1,14 @@
+# Self-elevating administrator rights https://stackoverflow.com/a/63344749
+$identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+$principal = [Security.Principal.WindowsPrincipal] $identity
+if(-not $principal.IsInRole( [Security.Principal.WindowsBuiltInRole]::Administrator )) {
+    $self = "-NoProfile -NoLogo -NoExit -File `"$($MyInvocation.MyCommand.Path)`" `"$($MyInvocation.MyCommand.UnboundArguments)`""
+    Start-Process -PassThru -FilePath PowerShell.exe -Verb Runas -ArgumentList $self
+    # Exiting PowerShell Terminal. https://stackoverflow.com/a/74359978
+    return [System.Environment]::Exit(0)
+}
+
+Set-Location "$($MyInvocation.MyCommand.Path | Split-Path -Parent)"
 Get-ChildItem .\functions\util -File | ForEach-Object { . $_.FullName }
 Get-ChildItem .\functions -File | ForEach-Object { . $_.FullName }
 
