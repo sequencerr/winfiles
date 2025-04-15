10527bf2a8fda63bac9b237465122510c27278cc
Sequencer <45060278+sequencerr@users.noreply.github.com>
2025-04-16 01:52:55 +0200

    Refactor performance

--- functions/tweaks-performance.ps1
+++ functions/tweaks-performance.ps1
@@ -26,7 +26,6 @@ function Invoke-PerfomanceOptionsDisable {
     Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects" `
     -Name "VisualFXSetting" -Value 3 -Type DWord
 
-    # This disables the following 8 settings:
     Write-Host 'Disable "Animate controls and elements inside windows"'
     Write-Host 'Disable "Fade or slide menus into view"'
     Write-Host 'Disable "Fade or slide ToolTips into view"'
@@ -37,32 +36,42 @@ function Invoke-PerfomanceOptionsDisable {
     Write-Host 'Disable "Smooth-scroll list boxes"'
     Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" `
     -Name "UserPreferencesMask" -Value (0x90, 0x12, 0x03, 0x80, 0x10, 0x00, 0x00, 0x00) -Type Binary
+
+    Write-Host 'Disable "Show window contents while dragging"'
+    Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" `
+    -Name "DragFullWindows" -Value 0 -Type DWord
+
+    Write-Host 'Enable  "Smooth edges of screen fonts"'
+    Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" `
+    -Name "FontSmoothing" -Value 2 -Type DWord
+
     Write-Host 'Disable "Animate windows when minimizing and maximizing"'
     Set-ItemProperty -Path "HKCU:\Control Panel\Desktop\WindowMetrics" `
-    -Name "MinAnimate" -Value "0"
-    Write-Host 'Disable "Animations in the taskbar"'
-    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" `
-    -Name "TaskbarAnimations" -Value 0 -Type DWord
+    -Name "MinAnimate" -Value 0 -Type DWord
+
     Write-Host 'Disable "Enable Peek"'
     Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\DWM" `
     -Name "EnableAeroPeek" -Value 0 -Type DWord
+
     Write-Host 'Disable "Save taskbar thumbnail previews"'
     Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\DWM" `
     -Name "AlwaysHibernateThumbnails" -Value 0 -Type DWord
+
+    Write-Host 'Disable "Animations in the taskbar"'
+    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" `
+    -Name "TaskbarAnimations" -Value 0 -Type DWord
+
     Write-Host 'Enable  "Show thumbnails instead of icons"'
     Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" `
     -Name "IconsOnly" -Value 0 -Type DWord
+
     Write-Host 'Disable "Show translucent selection rectangle"'
     Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" `
     -Name "ListviewAlphaSelect" -Value 0 -Type DWord
-    Write-Host 'Disable "Show window contents while dragging"'
-    Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" `
-    -Name "DragFullWindows" -Value "0"
-    Write-Host 'Enable  "Smooth edges of screen fonts"'
-    Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" `
-    -Name "FontSmoothing" -Value "2"
+
     Write-Host 'Disable "Use drop shadows for icon labels on the desktop"'
     Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" `
     -Name "ListviewShadow" -Value 0 -Type DWord
+
     Write-Host 'Most of "UserPreferencesMask" may not be displayed as changed until system restart'
 }
