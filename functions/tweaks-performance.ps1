0d3a22705b9917c2771a9764004025aefb41c6a5
Sequencer <45060278+sequencerr@users.noreply.github.com>
2025-05-04 13:27:26 +0200

    Refactor UserPreferences toggle. Add util

--- functions/tweaks-performance.ps1
+++ functions/tweaks-performance.ps1
@@ -1,25 +1,6 @@
 # https://superuser.com/a/1419689
 # https://www.elevenforum.com/t/enable-or-disable-font-smoothing-in-windows-11.8476
-
 # SystemPropertiesPerformance.exe
-
-# Animate controls and elements inside windows
-# Animate windows when minimizing and maximizing
-# Animations in the taskbar
-# Enable Peek
-# Fade or slide menus into view
-# Fade or slide ToolTips into view
-# Fade out menu items after clicking
-# Save taskbar thumbnail previews
-# Show shadows under mouse pointer
-# Show shadows under windows
-# Show thumbnails instead of icons
-# Show translucent selection rectangle
-# Show window contents while dragging
-# Slide open combo boxes
-# Smooth edges of screen fonts
-# Smooth-scroll list boxes
-# Use drop shadows for icon labels on the desktop
 
 function Invoke-PerfomanceOptionsDisable {
     Write-Host 'Must set appearance options to "Custom"'
@@ -27,39 +8,38 @@ function Invoke-PerfomanceOptionsDisable {
     -Name "VisualFXSetting" -Value 3 -Type DWord
 
     Write-Host 'Disable "Animate controls and elements inside windows"'
-    Write-Host 'Disable "Fade or slide menus into view"'
-    Write-Host 'Disable "Fade or slide ToolTips into view"'
-    Write-Host 'Disable "Fade out menu items after clicking"'
-    Write-Host 'Disable "Show shadows under mouse pointer"'
-    Write-Host 'Disable "Show shadows under windows"'
-    Write-Host 'Disable "Slide open combo boxes"'
-    Write-Host 'Disable "Smooth-scroll list boxes"'
-    Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" `
-    -Name "UserPreferencesMask" -Value (0x90, 0x12, 0x03, 0x80, 0x10, 0x00, 0x00, 0x00) -Type Binary
-
-    Write-Host 'Disable "Show window contents while dragging"'
-    Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" `
-    -Name "DragFullWindows" -Value 0 -Type DWord
-
-    Write-Host 'Enable  "Smooth edges of screen fonts"'
-    Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" `
-    -Name "FontSmoothing" -Value 2 -Type DWord
+    Disable-UserPreference $UserPreferencesMask.ControlAnimation
 
     Write-Host 'Disable "Animate windows when minimizing and maximizing"'
     Set-ItemProperty -Path "HKCU:\Control Panel\Desktop\WindowMetrics" `
     -Name "MinAnimate" -Value 0 -Type DWord
 
+    Write-Host 'Disable "Animations in the taskbar"'
+    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" `
+    -Name "TaskbarAnimations" -Value 0 -Type DWord
+
     Write-Host 'Disable "Enable Peek"'
     Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\DWM" `
     -Name "EnableAeroPeek" -Value 0 -Type DWord
+
+    Write-Host 'Disable "Fade or slide menus into view"'
+    Disable-UserPreference $UserPreferencesMask.MenuAnimation
+
+    Write-Host 'Disable "Fade or slide ToolTips into view"'
+    Disable-UserPreference $UserPreferencesMask.ToolTipAnimation
+
+    Write-Host 'Disable "Fade out menu items after clicking"'
+    Disable-UserPreference $UserPreferencesMask.SelectionFade
 
     Write-Host 'Disable "Save taskbar thumbnail previews"'
     Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\DWM" `
     -Name "AlwaysHibernateThumbnails" -Value 0 -Type DWord
 
-    Write-Host 'Disable "Animations in the taskbar"'
-    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" `
-    -Name "TaskbarAnimations" -Value 0 -Type DWord
+    Write-Host 'Disable "Show shadows under mouse pointer"'
+    Disable-UserPreference $UserPreferencesMask.CursorShadow
+
+    Write-Host 'Disable "Show shadows under windows"'
+    Disable-UserPreference $UserPreferencesMask.DropShadow
 
     Write-Host 'Enable  "Show thumbnails instead of icons"'
     Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" `
@@ -68,6 +48,20 @@ function Invoke-PerfomanceOptionsDisable {
     Write-Host 'Disable "Show translucent selection rectangle"'
     Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" `
     -Name "ListviewAlphaSelect" -Value 0 -Type DWord
+
+    Write-Host 'Disable "Show window contents while dragging"'
+    Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" `
+    -Name "DragFullWindows" -Value 0 -Type DWord
+
+    Write-Host 'Disable "Slide open combo boxes"'
+    Disable-UserPreference $UserPreferencesMask.ComboBoxAnimation
+
+    Write-Host 'Enable  "Smooth edges of screen fonts"'
+    Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" `
+    -Name "FontSmoothing" -Value 2 -Type DWord
+
+    Write-Host 'Disable "Smooth-scroll list boxes"'
+    Disable-UserPreference $UserPreferencesMask.ListBoxSmoothScrolling
 
     Write-Host 'Disable "Use drop shadows for icon labels on the desktop"'
     Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" `
---
+++ functions/util/Toggle-UserPreferences.ps1
@@ -0,0 +1,55 @@
+$v = [Int64]1
+$UserPreferencesMask = [ordered]@{
+    ActiveWindowTracking       = ($v -shl  0)
+    MenuAnimation              = ($v -shl  1)
+    ComboBoxAnimation          = ($v -shl  2)
+    ListBoxSmoothScrolling     = ($v -shl  3)
+    GradientCaptions           = ($v -shl  4)
+    KeybordCues                = ($v -shl  5)
+    ActiveWindowTrackingZOrder = ($v -shl  6)
+    HotTracking                = ($v -shl  7)
+    # Reserved                 = ($v -shl  8)
+    MenuFade                   = ($v -shl  9)
+    SelectionFade              = ($v -shl 10)
+    ToolTipAnimation           = ($v -shl 11)
+    ToolTipFade                = ($v -shl 12)
+    CursorShadow               = ($v -shl 13)
+    Sonar                      = ($v -shl 14)
+    ClickLock                  = ($v -shl 15)
+    HideMousePointer           = ($v -shl 16)
+    VisualStyle                = ($v -shl 17)
+    DropShadow                 = ($v -shl 18)
+    #                                     ...
+    UIEffects                  = ($v -shl 31)
+    DisableOverlappedContent   = ($v -shl 32)
+    ControlAnimation           = ($v -shl 33)
+    ClearType                  = ($v -shl 34)
+    SpeechRecognition          = ($v -shl 35)
+}
+
+# There is more straightforward solution in terms how computers work
+# (to edit specific byte of byte[], without converting back and forth),
+# but imo this is more straightforward in terms how to program it, read code.
+function Toggle-UserPreference {
+    PARAM($userPreference, $action)
+
+    $upm = Get-ItemProperty -Path "HKCU:\Control Panel\Desktop" `
+    -Name "UserPreferencesMask" | Select-Object -ExpandProperty "UserPreferencesMask"
+    if ($action) {
+        $upm = [BitConverter]::GetBytes([BitConverter]::ToInt64($upm, 0) -bor $userPreference)
+    } else {
+        $upm = [BitConverter]::GetBytes([BitConverter]::ToInt64($upm, 0) -band -bnot $userPreference)
+    }
+
+    Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" `
+    -Name "UserPreferencesMask" -Value $upm -Type Binary
+}
+
+function Enable-UserPreference {
+    PARAM($p)
+    Toggle-UserPreference $p 1
+}
+function Disable-UserPreference {
+    PARAM($p)
+    Toggle-UserPreference $p 0
+}
